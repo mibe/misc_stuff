adc4efe30f0a73d6ef227f91ffc2a6e169f6a80d
Michael Bemmerl <mail@mx-server.de>
2013-05-15 23:51:09 +0200

    Some other usage help.

--- add.bat
+++ add.bat
@@ -6,7 +6,7 @@ SET AVRDUDE="avrdude"
 IF NOT "%AVR32_HOME%" == "" SET AVRDUDE="%AVR32_HOME%\bin\avrdude.exe"
 
 REM Simple batch script for calling avrdude with options for USBtinyISP
-REM (C) 2012 Michael Bemmerl
+REM (C) 2012, 2013 Michael Bemmerl
 REM License: WTFPL-2.0
 
 IF "%1" == "" GOTO help
@@ -15,8 +15,10 @@ IF "%1" == "" GOTO help
 GOTO exit
 
 :help
-echo You probably want to add the following options:
+echo You probably want to add at least the part option
 echo -p [partno]
+echo.
+echo and some other AVRDUDE command line option like
 echo -U flash:w:[file]
 GOTO exit;
 
