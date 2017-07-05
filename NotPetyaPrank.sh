b34df2f6732dbca24d4b800d272679075d5b6149
Michael Bemmerl <mail@mx-server.de>
2017-07-05 23:13:27 +0200

    NotPetya prank. Use at your own risk! 😉

---
+++ NotPetyaPrank.sh
@@ -0,0 +1,36 @@
+#/bin/sh
+
+while [ true ]; do
+
+PS1=""
+echo -e "\e[37;41m"
+clear
+
+echo " Ooops, your important files are encrypted."
+echo ""
+echo "▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄"
+echo " If you see this text, then your files are no longer accessible, because they"
+echo " have been encrypted.  Perhaps you are busy looking for a way to recover your"
+echo " files, but don't waste your time.  Nobody can recover your files without our"
+echo " decryption service."
+echo ""
+echo " We guarantee that you can recover all your files safely and easily.  All you"
+echo " need to do is submit the payment and purchase the decryption key."
+echo ""
+echo " Please follow the instructions:"
+echo ""
+echo " 1. Send \$300 worth of Bitcoin to following address:"
+echo ""
+echo "    1Mz7153HMuxXTuR2R1t78mGSdzaAtNbBWXX"
+echo ""
+echo ""
+echo " 2. Send your Bitcoin wallet ID and personal installation key to e-mail"
+echo "    wowsmith123456@posteo.net. Your personal installation key:"
+echo ""
+echo "56fkKs-w3Mgh9-kKkl82-Asdi43-w3wJEv-65P8rt-DuS4Fv9-CjFe6H-cwGZ2L-Dqgh6y-4d9s11"
+echo ""
+echo " If you already purchased your key, please enter it below."
+#echo " Key:"
+read -p " Key: " -n 999 -r
+
+done
